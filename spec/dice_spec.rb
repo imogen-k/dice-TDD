a9dd9ff03dce866ba00b7504ab8df10d6833ba6e
imogenkraak <imogen.kraak@gmail.com>
2020-08-06 11:27:58 +0100

    Roll method added

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,4 +3,9 @@ describe Dice do
   it 'Rolls a dice' do
     expect(Dice.new).to respond_to :roll
   end
+
+  it 'rolling dice returns 1' do
+    expect(subject.roll).to be_between(1, 6)
+  end
+
 end
