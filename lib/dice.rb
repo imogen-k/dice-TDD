a7c2aab0b67b612d5fb7adb07aba3e3ee3f480e8
imogenkraak <imogen.kraak@gmail.com>
2020-08-06 12:42:17 +0100

    Final unit test passed

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,5 +1,18 @@
 class Dice
+
+  @@array = []
+
   def roll
-    rand(1..6)
+   rand(1..6)
+  end
+
+  def number_of_dice(number_of_dice)
+    number_of_dice.times {
+      @@array << roll
+    }
+  end
+
+  def counter
+    @@array
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,11 +1,22 @@
 require 'dice'
+
 describe Dice do
-  it 'Rolls a dice' do
-    expect(Dice.new).to respond_to :roll
+
+  it 'rolls the dice' do
+    expect(Dice.new).to respond_to(:roll)
   end
 
-  it 'rolling dice returns 1' do
+  it 'rolling dice returns number between 1 & 6' do
     expect(subject.roll).to be_between(1, 6)
+  end
+
+  it 'person can roll as many dice as they want' do
+    subject.number_of_dice(3)
+    expect(subject.counter.count).to eq(3)
+  end
+
+  it 'final score' do
+    expect(subject).to respond_to(:counter)
   end
 
 end
